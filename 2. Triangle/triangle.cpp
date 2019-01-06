0e01d10fd3bd0f1c7202d8607655e1ab2f6c60a4
Madhav Kanbur <abcdjdj@gmail.com>
2019-01-06 15:55:03 +0530

    Use spaces for indentation
    Signed-off-by: Madhav Kanbur <abcdjdj@gmail.com>

--- 2. Triangle/triangle.cpp
+++ 2. Triangle/triangle.cpp
@@ -10,7 +10,7 @@ static const char *vShader =
 layout(location = 0) in vec3 pos;\n\
 \
 void main() {\n\
-	gl_Position = vec4(pos, 1.0);\n\
+    gl_Position = vec4(pos, 1.0);\n\
 }";
 
 static const char *fShader =
@@ -18,61 +18,61 @@ static const char *fShader =
 out vec4 color;\n\
 \
 void main() {\n\
-	color = vec4(1.0, 0.0, 1.0, 1.0);\n\
+    color = vec4(1.0, 0.0, 1.0, 1.0);\n\
 }";
 
 GLuint VAO, VBO, shader;
 
 void AddShader(GLuint theProgram, const char *shaderCode, GLenum shaderType)
 {
-	GLuint theShader = glCreateShader(shaderType);
+    GLuint theShader = glCreateShader(shaderType);
 
-	const GLchar **theCode = &shaderCode;
-	const GLint codeLength = strlen(shaderCode);
-	glShaderSource(theShader, 1, theCode, &codeLength);
-	glCompileShader(theShader);
+    const GLchar **theCode = &shaderCode;
+    const GLint codeLength = strlen(shaderCode);
+    glShaderSource(theShader, 1, theCode, &codeLength);
+    glCompileShader(theShader);
 
-	GLint result = 0;
-	GLchar elog[1024] = { 0 };
-	glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
-	if(!result) {
-		glGetShaderInfoLog(theShader, sizeof(elog), NULL, elog);
-		std::cout << "Error compiling" << shaderType << "shader : " << elog << std::endl;
-		return;
-	}
+    GLint result = 0;
+    GLchar elog[1024] = { 0 };
+    glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
+    if(!result) {
+        glGetShaderInfoLog(theShader, sizeof(elog), NULL, elog);
+        std::cout << "Error compiling" << shaderType << "shader : " << elog << std::endl;
+        return;
+    }
 
-	glAttachShader(theProgram, theShader);
+    glAttachShader(theProgram, theShader);
 }
 
 void CompileShaders()
 {
-	shader = glCreateProgram();
-	if(!shader) {
-		std::cout << "Error creating shader program!" << std::endl;
-		return;
-	}
+    shader = glCreateProgram();
+    if(!shader) {
+        std::cout << "Error creating shader program!" << std::endl;
+        return;
+    }
 
-	AddShader(shader, vShader, GL_VERTEX_SHADER);
-	AddShader(shader, fShader, GL_FRAGMENT_SHADER);
+    AddShader(shader, vShader, GL_VERTEX_SHADER);
+    AddShader(shader, fShader, GL_FRAGMENT_SHADER);
 
-	GLint result = 0;
-	GLchar elog[1024] = { 0 };
+    GLint result = 0;
+    GLchar elog[1024] = { 0 };
 
-	glLinkProgram(shader);
-	glGetProgramiv(shader, GL_LINK_STATUS, &result);
-	if(!result) {
-		glGetProgramInfoLog(shader, sizeof(elog), NULL, elog);
-		std::cout << "Error linking shader : " << elog << std::endl;
-		return;
-	}
+    glLinkProgram(shader);
+    glGetProgramiv(shader, GL_LINK_STATUS, &result);
+    if(!result) {
+        glGetProgramInfoLog(shader, sizeof(elog), NULL, elog);
+        std::cout << "Error linking shader : " << elog << std::endl;
+        return;
+    }
 
-	glValidateProgram(shader);
-	glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
-	if(!result) {
-		glGetProgramInfoLog(shader, sizeof(elog), NULL, elog);
-		std::cout << "Error validating shader : " << elog << std::endl;
-		return;
-	}
+    glValidateProgram(shader);
+    glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+    if(!result) {
+        glGetProgramInfoLog(shader, sizeof(elog), NULL, elog);
+        std::cout << "Error validating shader : " << elog << std::endl;
+        return;
+    }
 
 }
 
@@ -94,9 +94,9 @@ void CreateTriangle()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
     glEnableVertexAttribArray(0);
 
-	// Clear everything
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+    // Clear everything
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 }
 
 int main(void)
@@ -110,47 +110,47 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     // Core Profile = No Backwards Compatibility
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "My Triangle", NULL, NULL);
-	if (!mainWindow) {
-		std::cout << "GLFW window creation failed!" << std::endl;
-		glfwTerminate();
-		return 1;
-	}
+    if (!mainWindow) {
+        std::cout << "GLFW window creation failed!" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
 
-	// Set context for GLEW to use
-	glfwMakeContextCurrent(mainWindow);
+    // Set context for GLEW to use
+    glfwMakeContextCurrent(mainWindow);
 
-	// Allow modern extension features
-	glewExperimental = GL_TRUE;
+    // Allow modern extension features
+    glewExperimental = GL_TRUE;
 
-	if (glewInit() != GLEW_OK) {
-		std::cout << "GLEW initialisation failed!" << std::endl;
-		glfwDestroyWindow(mainWindow);
-		glfwTerminate();
-		return 1;
-	}
+    if (glewInit() != GLEW_OK) {
+        std::cout << "GLEW initialisation failed!" << std::endl;
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return 1;
+    }
 
-	CreateTriangle();
-	CompileShaders();
-	
+    CreateTriangle();
+    CompileShaders();
+    
     while (!glfwWindowShouldClose(mainWindow)) {
-		// Get + Handle user input events
-		glfwPollEvents();
+        // Get + Handle user input events
+        glfwPollEvents();
 
-		// Clear window
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+        // Clear window
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shader);
-		glBindVertexArray(VAO);
-		
-			glDrawArrays(GL_TRIANGLES, 0, 3);
-		
-		glBindVertexArray(0);
-		glUseProgram(0);
+        glUseProgram(shader);
+        glBindVertexArray(VAO);
+        
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        glBindVertexArray(0);
+        glUseProgram(0);
 
-		glfwSwapBuffers(mainWindow);
-	}
+        glfwSwapBuffers(mainWindow);
+    }
 }
